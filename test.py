1344ef67d4a2fcc01b96bbb66d1dfdbb81b4d3d1
marcinozog <m82ozog@gmail.com>
2024-04-18 12:06:21 +0200

    rev 0.93

--- test.py
+++
@@ -1,8 +0,0 @@
-n_classes = 3
-plot_colors = "ryb"
-
-for i, color in zip(range(n_classes), plot_colors):
-    print(i, color)
-
-for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]):
-    print(pairidx, pair)
